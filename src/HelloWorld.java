92b572e75906803196743925196c592b4ae73ddb
Greg Jett <greg.jett25@gmail.com>
2020-06-01 17:02:19 -0500

    java intro files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,56 @@
 public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 25;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hello, World!";
+        System.out.println(myString);
+
+//      float myNumber = 3.14L;
+//      System.out.println(myNumber);
+
+
+
+//        myString = 'c';
+//        myString = 3.14159;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+
+
+        System.out.println(myString);
+//        System.out.println(myNumber);
+//        System.out.println(three);
+
+//        rewrite to be shorthand
+        int a = 4;
+        a = a + 5;
+          a += 5;
+
+        int b = 3;
+        int c = 4;
+      c = c * x;
+        c *= b;
+
+//        int x = 10;
+//        int y = 2;
+//      x = x / y;
+//        x /= y;
+//      y = y - x;
+//        y -= x;
     }
 }
