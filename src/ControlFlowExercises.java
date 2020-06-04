d138399d7a2431569c514c7a10f6dd3d1df8218e
Greg Jett <greg.jett25@gmail.com>
2020-06-03 22:49:04 -0500

    add control flow exerise java file

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,29 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+       int i = 5;
+       while(i >= 15) {
+           System.out.println("i is " + i);
+           i++;
+       }
+//       do () {
+//
+//        }
+//        int n = 100;
+//
+////         fizzbuzz
+//        for (int i=1; i<=n; i++)
+//        {
+//            if (i%15==0)
+//                System.out.print("FizzBuzz"+" ");
+//
+//            else if (i%5==0)
+//                System.out.print("Buzz"+" ");
+//
+//            else if (i%3==0)
+//                System.out.print("Fizz"+" ");
+//
+//            else
+//                System.out.print(i+" ");
+//        }
+    }
+}
