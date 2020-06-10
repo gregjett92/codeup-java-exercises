c8f2eba4eba4b10cd4123c16db2684b214dd416f
Greg Jett <greg.jett25@gmail.com>
2020-06-10 16:30:26 -0500

    dish file

---
+++ src/Dish.java
@@ -0,0 +1,12 @@
+public class Dish {
+    public int costInCents;
+    public String nameOfDish;
+    public boolean wouldRecommend;
+    static void printSummary() {
+        System.out.println(costInCents);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
