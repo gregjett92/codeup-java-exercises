51cebb4d07c6c356eb8f327ef6a9825d39a24644
Greg Jett <greg.jett25@gmail.com>
2020-06-22 14:08:57 -0500

    io files

---
+++ src/Files.java
@@ -0,0 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Paths;
+
+public class Files {
+
+    public static void main(String[] args) {
+        Path gitignore = Path.get(".gitignore");
+
+    }
+}
